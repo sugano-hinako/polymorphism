6ddffefe51603eb326fd05fadc207b853cd46ce1
hinako <hinako@ndg-no-mbp>
2020-10-16 14:58:00 +0900

    System.out.println追記

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,21 +14,25 @@ public class Main {
 		c[0] = new Hero("スラリン", 300);
 		c[1] = new Wizard("ミイホン", 200, 80);
 		c[2] = new Wizard();
+		//Wizardクラスの変数wにc[1]を代入
+		Wizard w = (Wizard)c[1];
 
 		//モンスターを生成
-		Monster [] monsters = new Monster[2];
-		monsters[0] = new Momonjya("ももんじゃ", 100);
-		monsters[1] = new Momonjya_ver2();
+		Monster [] m = new Monster[2];
+		m[0] = new Momonjya("ももんじゃ", 100);
+		m[1] = new Momonjya_ver2();
+		//Momonjya_ver2クラスの変数m2にm[1]を代入
+		Momonjya_ver2 m2 = (Momonjya_ver2)m[1];
 
+		//冒険スタート
 		System.out.println(c[0].name + "たちは草むらを歩いている");
 		System.out.println("草むらから敵が飛びだしてきた！！");
-		System.out.println("▼" + monsters[0].name+ "と" + monsters[1].name+ "  " +"があらわれた！");
-		c[0].attack(monsters[0]);
-		((Wizard) c[1]).fireball(monsters[1]);
-		/*if (c[1] instanceof Wizard) {//もしc[1]の中身がWizardと見なして大丈夫なら
-			Wizard w = (Wizard)c[1];   //Wizardと見なして
-			c[1].fireball(monsters[1]);//Fireballメソッドを使えるようにしたい
-		}*/
+		System.out.println("▼" + m[0].name+ "と" + m[1].name+ "  " +"があらわれた！");
+		c[0].attack(m[0]);
+		w.fireball(m[1]);
+		System.out.println("▼敵のターン");
+		m2.rollOver(c[0]);
+
 	}
 
 }
